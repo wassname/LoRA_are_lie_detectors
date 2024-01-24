8c47130a129560e0f6a654de2630a91718937a01
wassname <git@wassname.org>
2024-01-25 06:09:01 +0800

    wip

--- src/iris/tokenizer.py
+++ src/iris/tokenizer.py
@@ -9,6 +9,8 @@ from einops import rearrange
 from torch import Tensor
 import torch
 import torch.nn as nn
+from src.vae.sae2 import AutoEncoderConfig, Encoder, Decoder, Affines
+
 
 # from .lpips import LPIPS
 from .nets import Encoder, Decoder
@@ -36,21 +38,27 @@ class TokenizerEncoderOutput:
 Input = Float[Tensor, 'batch layers act']
 
 
+
 class Tokenizer(nn.Module):
-    def __init__(self, vocab_size: int, embed_dim: int, tokens_per_layer:int , encoder: Encoder, decoder: Decoder) -> None:
+    def __init__(self, vocab_size: int, embed_dim: int, tokens_per_layer:int , encoder: Encoder, decoder: Decoder, norm: Affines, importance_matrix=None) -> None:
         super().__init__()
         self.vocab_size = vocab_size
         self.tokens_per_layer = tokens_per_layer
+        self.importance_matrix = importance_matrix
 
         self.encoder = encoder
         self.embedding = nn.Embedding(vocab_size, embed_dim)
         self.decoder = decoder
+        self.norm = norm
         self.embedding.weight.data.uniform_(-1.0 / vocab_size, 1.0 / vocab_size)
 
+
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor]:
+        x = self.norm(x)
         outputs = self.encode(x)
         decoder_input = outputs.z + (outputs.z_quantized - outputs.z).detach()
         reconstructions = self.decode(decoder_input)
+        reconstructions = self.norm.inv(reconstructions)
         return outputs.z, outputs.z_quantized, reconstructions
 
     def compute_loss(self, x: Input, **kwargs: Any) -> LossWithIntermediateLosses:
@@ -62,7 +70,10 @@ class Tokenizer(nn.Module):
         beta = 1.0
         commitment_loss = (z.detach() - z_quantized).pow(2).mean() + beta * (z - z_quantized.detach()).pow(2).mean()
 
-        reconstruction_loss = torch.abs(x - x_rec).mean()
+        reconstruction_loss = torch.abs(x - x_rec)
+        if self.importance_matrix is not None:
+            reconstruction_loss = reconstruction_loss * self.importance_matrix
+        reconstruction_loss = reconstruction_loss.mean()
 
         return LossWithIntermediateLosses(commitment_loss=commitment_loss, reconstruction_loss=reconstruction_loss)
 
@@ -90,3 +101,51 @@ class Tokenizer(nn.Module):
         z_q = self.encode(x).z_quantized
         return self.decode(z_q)
 
+
+
+class TokenizedAutoEncoder(nn.Module):
+    def __init__(self, c_in, vocab_size:int=512, embed_dim:int=512, tokens_per_layer:int=4, encoder_sizes=[32,32], importance_matrix=None,) -> None:
+        super().__init__()
+        n_instances, n_input_ae = c_in
+        self.ae_cfg = AutoEncoderConfig(
+            n_instances=n_instances,
+            n_input_ae=n_input_ae,
+            n_hidden_ae=vocab_size * tokens_per_layer, # output size of decoder (tokens)
+            encoder_sizes=encoder_sizes,
+        )
+        encoder = Encoder(self.ae_cfg)
+
+        self.ae_cfg = AutoEncoderConfig(
+            n_instances=n_instances,
+            n_input_ae=n_input_ae,
+            n_hidden_ae=embed_dim * tokens_per_layer, # input channel to decoder (embedded tokens)
+            encoder_sizes=encoder_sizes,
+        )
+        decoder = Decoder(self.ae_cfg)
+        norm = Affines(n_instances, n_input_ae)
+
+        self.tokenizer = Tokenizer(
+            vocab_size=vocab_size,
+            embed_dim=embed_dim,
+            tokens_per_layer=tokens_per_layer,
+            encoder=encoder,
+            decoder=decoder,
+            norm=norm,
+            importance_matrix=importance_matrix,
+        )
+        
+    def forward(self, x):
+        o = self.tokenizer.encode(x)
+        z, z_q, tokens = o.z, o.z_quantized, o.tokens
+        h_rec = self.tokenizer.decode(z_q)
+        losses = self.tokenizer.compute_loss(x)
+        loss = losses.loss_total
+        
+        return dict(
+            loss=loss,
+            z=z, 
+            z_q=z_q,
+            tokens=tokens,
+            h_rec=h_rec,
+            **losses.intermediate_losses
+        )
--- src/vae/tae.py
+++ src/vae/tae.py
@@ -9,6 +9,7 @@ from src.vae.conv_inception import (
     auroc,
 )
 from src.vae.sae2 import AutoEncoderConfig, Encoder, Decoder, Affines
+from src.iris.tokenizer import TokenizedAutoEncoder
 from einops import reduce
 import torch
 import torch.nn as nn
@@ -23,11 +24,9 @@ class PL_TAE(PLBase):
         lr=4e-3,
         weight_decay=0,
         n_latent=512,
-        dropout=0,
         encoder_sizes=[32, ],
         importance_matrix=None,
         tokens_per_layer=6,
-        probe_embedd_dim=3,
         **kwargs,
     ):
         super().__init__(
@@ -37,45 +36,100 @@ class PL_TAE(PLBase):
             weight_decay=weight_decay,
         )
         self.save_hyperparameters()
+        
         self.importance_matrix = importance_matrix
 
-        self.vocab_size = vocab_size = n_latent
-        self.embed_dim = embed_dim = n_latent
+        self.tae = TokenizedAutoEncoder(
+            c_in,
+            vocab_size=n_latent,
+            embed_dim=n_latent,
+            tokens_per_layer=tokens_per_layer,
+            encoder_sizes=encoder_sizes,
+            importance_matrix=importance_matrix,
+        )
+
+    def forward(self, x):
+        o = self.ae.encode(x)
+        z, z_q, tokens = o.z, o.z_quantized, o.tokens
+        h_rec = self.ae.decode(z_q)
+        losses = self.ae.compute_loss(x)
+        loss = losses.loss_total
+        
+        return dict(
+            loss=loss,
+            z=z, 
+            z_q=z_q,
+            tokens=tokens,
+            h_rec=h_rec,
+            **losses.intermediate_losses
+        )
 
 
+    def _step(self, batch, batch_idx, stage="train"):
+        device = next(self.parameters()).device
+        x, y = batch
+        x = x.to(device)
+        y = y.to(device)
+        x0 = x  # [..., 0]
+        info0 = self(x0)
+
+        rec_loss = info0["loss"]
+
+        self.log(
+            f"{stage}/loss_rec",
+            float(rec_loss),
+            on_epoch=True,
+            on_step=True,
+        )
+        self.log(f"{stage}/commitment_loss", info0['commitment_loss'], on_epoch=True, on_step=False)
+        self.log(f"{stage}/reconstruction_loss", info0['reconstruction_loss'], on_epoch=True, on_step=False)
+        self.log(
+            f"{stage}/n",
+            float(len(y)),
+            on_epoch=True,
+            on_step=False,
+            reduce_fx=torch.sum,
+        )
+        
+        assert torch.isfinite(rec_loss), "rec_loss is not finite"
+        return rec_loss
+
+
+
+class PL_TAEProbe(PLBase):
+    def __init__(
+        self,
+        c_in,
+        tae: TokenizedAutoEncoder,
+        steps_per_epoch,
+        max_epochs,
+        lr=4e-3,
+        weight_decay=0,
+        dropout=0,
+        probe_embedd_dim=3,
+        **kwargs,
+    ):
+        super().__init__(
+            steps_per_epoch=steps_per_epoch,
+            max_epochs=max_epochs,
+            lr=lr,
+            weight_decay=weight_decay,
+        )
+        self.save_hyperparameters(exclude=["tae"])
+        self.tae = tae
+        
+        # extract config from tokenized autoencoder
+        tokens_per_layer = tae.tokenizer.tokens_per_layer
+        embed_dim = tae.tokenizer.embed_dim
+        vocab_size = tae.tokenizer.vocab_size
         n_layers, n_channels = c_in
         
-        # move to ae?
-        self.norm = Affines(n_layers, n_channels)
-        
-        self.ae_cfg = AutoEncoderConfig(
-            n_instances=n_layers,
-            n_input_ae=n_channels,
-            n_hidden_ae=vocab_size * tokens_per_layer, # output size of decoder (tokens)
-            encoder_sizes=encoder_sizes,
-        )
-        encoder = Encoder(self.ae_cfg)
+        # self.tae.encoder.cfg
 
-        self.ae_cfg = AutoEncoderConfig(
-            n_instances=n_layers,
-            n_input_ae=n_channels,
-            n_hidden_ae=embed_dim * tokens_per_layer, # input channel to decoder (embedded tokens)
-            encoder_sizes=encoder_sizes,
-        )
-        decoder = Decoder(self.ae_cfg)
-        
-
-        self.ae = Tokenizer(
-            vocab_size=vocab_size,
-            embed_dim=embed_dim,
-            tokens_per_layer=tokens_per_layer,
-            encoder=encoder,
-            decoder=decoder,
-        )
+        # self.vocab_size = vocab_size = n_latent
+        # self.embed_dim = embed_dim = n_latent
 
 
-        # self.only_emb = False
-        
         # if self.only_emb:
         #     self.head = nn.Sequential(
         #         nn.Embedding(vocab_size, probe_embedd_dim, 
@@ -98,9 +152,8 @@ class PL_TAE(PLBase):
         #         Rearrange("b l -> (b l)"), 
         #     )
             
-        # use z_q
-        probe_embedd_dim = vocab_size
-        n = n_layers * tokens_per_layer * probe_embedd_dim
+        # use z_q?
+        n = n_layers * tokens_per_layer * vocab_size
         self.head = nn.Sequential(
             Rearrange("b l h v -> b (l h v)"),   
             # LinBnDrop(n*embed_dim2, n, bn=True, dropout=dropout),
@@ -110,50 +163,18 @@ class PL_TAE(PLBase):
             nn.Linear(n, 1),
             Rearrange("b l -> (b l)"), 
         )
-        self._ae_mode = True
-
-    def ae_mode(self, mode=0):
-        """
-        mode 0, train the ae
-        mode 1, train only the prob
-        mode 2, train both
-        """
-        if mode == 0:
-            print("training ae")
-        elif mode == 1:
-            print("training probe")
-        elif mode == 2:
-            print("training both ae and probe")
-        self._ae_mode = mode
-        recursive_requires_grad(self.ae, mode in [0, 2])
 
     def forward(self, x):
-        x = self.norm(x)
-        o = self.ae.encode(x)
+        with torch.no_grad():
+            o = self.ae.encode(x)
+        
         z, z_q, tokens = o.z, o.z_quantized, o.tokens
-        h_rec = self.ae.decode(z_q)
-        h_rec = self.norm.inv(h_rec)
-        losses = self.ae.compute_loss(x)
-        loss = losses.loss_total
 
         # we want a probe tokenizer. reusing the decoder hurts performance
-        # latent_decoder = self.ae.decode(z_q)
-        # latent_probe = self.head[0](tokens)
-        # pred = self.head(tokens)#.squeeze(1)
+        pred = self.head(tokens)
         
         pred = self.head(z_q)
-        
-        
-        return dict(
-            pred=pred,
-            loss=loss,
-            # latent_probe=latent_probe,
-            z=z, 
-            z_q=z_q,
-            tokens=tokens,
-            h_rec=h_rec,
-            **losses.intermediate_losses
-        )
+        return pred
 
 
     def _step(self, batch, batch_idx, stage="train"):
@@ -163,57 +184,37 @@ class PL_TAE(PLBase):
         x = x.to(device)
         y = y.to(device)
         x0 = x  # [..., 0]
-        info0 = self(x0)
-        logits = info0["pred"]
         
+        logits = self(x0)        
         
         # if using head
-        # if self.only_emb:
-        #     y_probs = logits
-        # else:
-        y_probs = F.sigmoid(logits)
+        if self.only_emb:
+            y_probs = logits
+        else:
+            y_probs = F.sigmoid(logits)
         
         assert (y_probs >= 0).all(), "y_probs should be positive"
         assert (y_probs <= 1).all(), "y_probs should be less than 1"
-        
-        y_cls = y_probs > 0.5
 
         if stage == "pred":
             return (y_probs).float()
 
         pred_loss = F.binary_cross_entropy_with_logits(y_probs, (y > y_thresh).float())
-
+        # OR
         pred_loss = F.smooth_l1_loss(y_probs, y)
-        rec_loss = info0["loss"]
-
-        if self._ae_mode in [1, 2]:
-            self.log(
-                f"{stage}/auroc",
-                auroc(y_probs, y > y_thresh, "binary"),
-                on_epoch=True,
-                on_step=False,
-            )
-            self.log(
-                f"{stage}/acc",
-                accuracy(y_cls, y > y_thresh, "binary"),
-                on_epoch=True,
-                on_step=False,
-            )
-            self.log(
-                f"{stage}/loss_pred",
-                float(pred_loss),
-                on_epoch=True,
-                on_step=True,
-            )
-        if self._ae_mode in [0, 2]:
-            self.log(
-                f"{stage}/loss_rec",
-                float(rec_loss),
-                on_epoch=True,
-                on_step=True,
-            )
-            self.log(f"{stage}/commitment_loss", info0['commitment_loss'], on_epoch=True, on_step=False)
-            self.log(f"{stage}/reconstruction_loss", info0['reconstruction_loss'], on_epoch=True, on_step=False)
+        
+        self.log(
+            f"{stage}/auroc",
+            auroc(y_probs, y > y_thresh, "binary"),
+            on_epoch=True,
+            on_step=False,
+        )
+        self.log(
+            f"{stage}/loss_pred",
+            float(pred_loss),
+            on_epoch=True,
+            on_step=True,
+        )
         self.log(
             f"{stage}/n",
             float(len(y)),
@@ -221,15 +222,5 @@ class PL_TAE(PLBase):
             on_step=False,
             reduce_fx=torch.sum,
         )
-        
-        if self._ae_mode == 0:
-            assert torch.isfinite(rec_loss), "rec_loss is not finite"
-            return rec_loss
-        elif self._ae_mode == 1:
-            assert torch.isfinite(pred_loss), "pred_loss is not finite"
-            return pred_loss
-        elif self._ae_mode == 2:
-            # , train/loss_pred_epoch=0.0195, train/loss_rec_epoch=169.0
-            assert torch.isfinite(pred_loss), "pred_loss is not finite"
-            assert torch.isfinite(rec_loss), "rec_loss is not finite"
-            return pred_loss * 50000 + rec_loss
+        assert torch.isfinite(pred_loss), "pred_loss is not finite"
+        return pred_loss
